de22e4eb92c0eae778f342c52bb6a1800e35196a
adamziolk <56138895+adamziolk@users.noreply.github.com>
2019-11-09 11:56:42 -0600

    Updates

---
+++ Principle Component Analysis and Kernel Density Estimation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 11:51:22 2019
+
+@author: Adam-PC
+"""
+
+data.columns
+data['fighter'].value_counts().hist()
+data['fighter'].value_counts().mean()
